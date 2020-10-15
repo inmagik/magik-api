5c36e6df41bf43507a2fbdc03cee9cb3c08dfd70
gffuma <fumagalli.gf@gmail.com>
2020-10-15 10:06:05 +0200

    Improve methods docs

--- src/index.ts
+++ src/index.ts
@@ -194,19 +194,58 @@ abstract class BaseApiBuilder<T> {
     })
   }
 
+  /**
+   * Set the base url for your next http requests
+   * return a new instance of furrent api builder object
+   *
+   * @param {string} baseUrl Base url for all your requests
+   **/
   baseUrl = (baseUrl: string) => this.clone({ ...this.options, baseUrl })
 
+  /**
+   * Inject the auhentication into your http request
+   * according to your authHeaders() config
+   *
+   * @param {any} auth A value that rapresent your auth usually a token
+   **/
   auth = (auth: any) => this.clone({ ...this.options, auth })
 
+  /**
+   * Describe how to inject the authentication headers into your http request
+   * when called using .auth() method.
+   *
+   * @param {AuthHeadersInjector} injectAuthHeaders A callback that is supposed to return
+   * an object of headers using to authenticate your request.
+   *
+   * (token) => { 'Authorization': `Token ${token}` }
+   **/
   authHeaders = (injectAuthHeaders: AuthHeadersInjector) =>
     this.clone({ ...this.options, injectAuthHeaders })
 
+  /**
+   * Map the response from rxjs ajax result to the result of your
+   * http request.
+   *
+   * @param {ResponseMapper} mapResponse A callback to map your response
+   *
+   * (r) => r.response.map(...)
+   **/
   mapResponse = (mapResponse: ResponseMapper) =>
     this.clone({ ...this.options, mapResponse })
 
+  /**
+   * Enable or disable trailing slash
+   *
+   * @param {boolean} trailingSlash Is trailing slash enabled?
+   */
   trailingSlash = (trailingSlash: boolean) =>
     this.clone({ ...this.options, trailingSlash })
 
+  /**
+   * Set the rxjs AJAX request configuration
+   *
+   * @param {ApiUrlRequest} requestConfig The rxjs request config object
+   */
   request = (requestConfig: ApiUrlRequest) =>
     this.clone({
       ...this.options,
@@ -216,6 +255,11 @@ abstract class BaseApiBuilder<T> {
       },
     })
 
+  /**
+   * Set the headers of your http request, merge with previous setted headers
+   *
+   * @param {Object} headers An object of headers
+   */
   headers = (headers: Object) =>
     this.clone({
       ...this.options,
@@ -228,6 +272,11 @@ abstract class BaseApiBuilder<T> {
       },
     })
 
+  /**
+   * Set the query string of your http request
+   *
+   * @param {ParsedQuery} queryParams An object encoded as query string
+   */
   query = (queryParams: ParsedQuery) =>
     this.clone({
       ...this.options,
@@ -239,14 +288,44 @@ abstract class BaseApiBuilder<T> {
 }
 
 abstract class HttpVerbsApiBuilder<T> extends BaseApiBuilder<T> {
+  /**
+   * Make a GET HTTP Request
+   *
+   * @param {string} url The url of request
+   * @param {ParsedQuery} query An object encoded as query string
+   **/
   get = (url: string, query?: ParsedQuery) => httpGET(this.options, url, query)
 
+  /**
+   * Make a POST HTTP Request
+   *
+   * @param {string} url The url of request
+   * @param {any} body The body of request
+   **/
   post = (url: string, body?: any) => httpPOST(this.options, url, body)
 
+  /**
+   * Make a PUT HTTP Request
+   *
+   * @param {string} url The url of request
+   * @param {any} body The body of request
+   **/
   put = (url: string, body?: any) => httpPUT(this.options, url, body)
 
+  /**
+   * Make a PATCH HTTP Request
+   *
+   * @param {string} url The url of request
+   * @param {any} body The body of request
+   **/
   patch = (url: string, body?: any) => httpPATCH(this.options, url, body)
 
+  /**
+   * Make a DELETE HTTP Request
+   *
+   * @param {string} url The url of request
+   * @param {ParsedQuery} query An object encoded as query string
+   **/
   delete = (url: string, query?: ParsedQuery) =>
     httpDELETE(this.options, url, query)
 }
@@ -277,22 +356,58 @@ class UrlApiBuilder extends BaseApiBuilder<UrlApiBuilder> {
     super(options)
   }
 
+  /**
+   * Create an API Builder instance with auth curried
+   *
+   **/
   curryAuth = () => new ApiCurriedAuthUrlBuilder(this.options)
 
+  /**
+   * Create an API Builder instance with given url concat with current
+   * url as curried url
+   *
+   * @param {string} url The curried url
+   **/
   url = (url: string) =>
     new UrlApiBuilder({ ...this.options, url: this.options.url + url })
 
   resource = (url: string) =>
     new ResourceApiBuilder({ ...this.options, url: this.options.url + url })
 
+  /**
+   * Make a GET HTTP Request to your current curried url
+   *
+   * @param {ParsedQuery} query An object encoded as query string
+   **/
   get = (query?: ParsedQuery) => httpGET(this.options, '', query)
 
+  /**
+   * Make a POST HTTP Request to your current curried url
+   *
+   * @param {any} body The body of request
+   **/
   post = (body?: any) => httpPOST(this.options, '', body)
 
+  /**
+   * Make a PUT HTTP Request to your current curried url
+   *
+   * @param {string} url The url of request
+   * @param {any} body The body of request
+   **/
   put = (body?: any) => httpPUT(this.options, '', body)
 
+  /**
+   * Make a PATCH HTTP Request to your current curried url
+   *
+   * @param {any} body The body of request
+   **/
   patch = (body?: any) => httpPATCH(this.options, '', body)
 
+  /**
+   * Make a DELETE HTTP Request to your current curried url
+   *
+   * @param {ParsedQuery} query An object encoded as query string
+   **/
   delete = (query?: ParsedQuery) => httpDELETE(this.options, '', query)
 
   protected clone = (options: ApiOptions) => new UrlApiBuilder(options)
@@ -339,22 +454,64 @@ class CurriedAuthResourceApiBuilder extends BaseApiBuilder<
 class ResourceApiBuilder extends HttpVerbsApiBuilder<ResourceApiBuilder> {
   curryAuth = () => new CurriedAuthResourceApiBuilder(this.options)
 
+  /**
+   * Make a GET request to your configured url
+   *
+   * @param {ParsedQuery} query An object encoded as query string
+   */
   list = (query?: ParsedQuery) => httpGET(this.options, '', query)
 
+  /**
+   * Make a GET request to your configured url/{pk}
+   *
+   * @param {number} pk Primary key of your resource
+   * @param {ParsedQuery} query An object encoded as query string
+   */
   detail = (pk: string | number, query?: ParsedQuery) =>
     httpGET(this.options, `/${pk}`, query)
 
+  /**
+   * Make a POST request to your configured url
+   *
+   * @param {any} body The body of request
+   */
   create = (body?: any) => httpPOST(this.options, '', body)
 
+  /**
+   * Make a PUT request to your configured url/{pk}
+   *
+   * @param {number} pk Primary key of your resource
+   * @param {any} body The body of request
+   */
   update = (pk: string | number, body?: any) =>
     httpPUT(this.options, `/${pk}`, body)
 
+  /**
+   * Make a PATCH request to your configured url/{pk}
+   *
+   * @param {number} pk Primary key of your resource
+   * @param {any} body The body of request
+   */
   partialUpdate = (pk: string | number, body?: any) =>
     httpPATCH(this.options, `/${pk}`, body)
 
+  /**
+   * Make a DELETE request to your configured url/{pk}
+   *
+   * @param {number} pk Primary key of your resource
+   * @param {ParsedQuery} query An object encoded as query string
+   */
   remove = (pk: string | number, query?: ParsedQuery) =>
     httpDELETE(this.options, `/${pk}`, query)
 
+  /**
+   * Make a DELETE request to your configured url/{pk}
+   *
+   * automatic map response as ({ id })
+   *
+   * @param {number} pk Primary key of your resource
+   * @param {ParsedQuery} query An object encoded as query string
+   */
   removeId = (id: string | number, query?: ParsedQuery) =>
     httpDELETE(
       { ...this.options, mapResponse: () => ({ id }) },
@@ -366,8 +523,18 @@ class ResourceApiBuilder extends HttpVerbsApiBuilder<ResourceApiBuilder> {
 }
 
 class ApiBuilder extends HttpVerbsApiBuilder<ApiBuilder> {
+  /**
+   * Create an API Builder instance with given url curried
+   *
+   * @param {string} url The curried url
+   **/
   url = (url: string) => new UrlApiBuilder({ ...this.options, url })
 
+  /**
+   * Create an API Resource Builder instance starting from given url
+   *
+   * @param {string} url The url of your resource
+   **/
   resource = (url: string) => new ResourceApiBuilder({ ...this.options, url })
 
   protected clone = (options: ApiOptions) => new ApiBuilder(options)
